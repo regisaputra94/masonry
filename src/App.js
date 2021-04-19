5d0c5213be6b25ed9abf9d388bde0394c11b53c7
Regi Saputra <regi.saputra@mola.tv>
2021-04-20 01:30:59 +0700

    masonry

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { data } from "./source";
+import { useMemo, useState, useCallback } from "react";
+import debounce from "./debounce";
 
-function App() {
+export default function App() {
+  const [searchText, setSearchText] = useState("");
+  const debouncedSearch = useCallback(debounce(setSearchText, 800), []);
+
+  const filteredItems = useMemo(() => {
+    const searchReg = new RegExp(searchText.toLowerCase());
+    return data
+      .filter((e) => searchReg.test(e.name.toLowerCase()))
+      .sort((a, b) => {
+        const aText = a.name.toLowerCase();
+        const bText = b.name.toLowerCase();
+        return aText < bText ? -1 : aText > bText ? 1 : 0;
+      });
+  }, [searchText]);
+
+  const handleKeyUp = (event) => {
+    debouncedSearch(event.target.value || '');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <input type="text" onChange={handleKeyUp} />
       </header>
+      <div className="masonry">
+        {filteredItems.map((e) => (
+          <div
+            style={{ "grid-row-end": "span 30" }}
+            className="brick"
+            key={e._id}
+          >
+            <img src={e.src} alt={e.name} />
+            <h3>{e.name.toUpperCase()}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/debounce.js
@@ -0,0 +1,16 @@
+function debounce(callback, wait) {
+    let timeout = null
+    return function() {
+      const callNow = !timeout
+      const next = () => callback.apply(this, arguments)
+      clearTimeout(timeout)
+      timeout = setTimeout(next, wait)
+  
+      if (callNow) {
+        next()
+      }
+    }
+  }
+  
+  export default debounce
+  
